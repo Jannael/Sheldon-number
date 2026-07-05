1603df5b43ca4306799a616275fd9be70564e8e4
Jannael <orlandojannael@gmail.com>
2026-07-05 10:20:26 -0600

    init with the conditions for a number to be a sheldon number

---
+++ index.ts
@@ -0,0 +1,18 @@
+// Sheldon number has the following conditions:
+// 1. it is a prime number
+// 2. its mirror is also a prime number
+// 3. the mirror of the position of the number in the prime numbers is the mirror of the mirror of the position of the number in the prime numbers
+// 4. the position of the number in the prime numbers is the product of its digits
+// 5. it is a palindrome number in binary
+
+// EXAMPLE: 73
+// 1. 73 is a prime number -> 21 in the position of prime numbers
+// 2. 37 it's mirror -> 12 in the position of prime numbers
+// 3. 12 mirror is 21
+// 4. 21 is the product of multiply 7 * 3
+// 5. 73 is a palindrome number in binary -> 1001001
+
+// Function to check if a number is prime if so then returns the position of the number in the prime numbers otherwise returns null
+function isPrime(num: number): number | null {
+  return 0
+}
